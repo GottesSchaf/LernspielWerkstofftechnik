1e98b339e0d8678b6cacc1383de4286bcde66b12
Hannah <hanndragggon@gmail.com>
2018-10-10 14:16:00 +0200

    Rooms 1-3 updated, trying to animate a door... didnt worked :( Adding little Images and Models, new Bookshelf over there!

---
+++ Spiel23.03.2018/Assets/scripts/DoorHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour {
+    private Animator animator = null; 
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("isopen", true);
+        }
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        animator.SetBool("isopen", false); 
+    }
+}
