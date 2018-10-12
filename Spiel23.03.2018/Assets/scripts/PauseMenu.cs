83b13bf34d93e66a550814e6d5c27a7cbc4ffeb0
Hannah Pollow <hanndragggon@gmail.com>
2018-10-12 15:22:46 +0200

    Skybox replaced with solid color

---
+++ Spiel23.03.2018/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;  //Pausescreen Image
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+        void Resume() {
+        pauseMenuUI.SetActive(false); 
+        Time.timeScale = 1f;   // Normal Game Time 
+        GameIsPaused = false; 
+        }
+
+        void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;  // Whole Game is paused while the screen is open
+        GameIsPaused = true; 
+        }
+	}
+
